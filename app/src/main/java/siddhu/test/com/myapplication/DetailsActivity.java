a64358900c6b137fa7dc8b01bc9624fb15faf4a5
shdhumale@gmail.com <shdhumale@gmail.com>
2016-08-04 17:26:42 +0530

    Siddhu Commit

--- app/src/main/java/siddhu/test/com/myapplication/DetailsActivity.java
+++ app/src/main/java/siddhu/test/com/myapplication/DetailsActivity.java
@@ -2,10 +2,13 @@ package siddhu.test.com.myapplication;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import siddhu.test.com.myapplication.objects.NewsObjects;
 
@@ -16,6 +19,7 @@ import siddhu.test.com.myapplication.objects.NewsObjects;
 public class DetailsActivity extends AppCompatActivity {
 
     WebView webView;
+    ProgressBar progressBar;
 
     private static final int DEFAULT_POSITION=-1;
     private static final String KEY_POSITION="position";
@@ -24,11 +28,25 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_detail);
+        progressBar = (ProgressBar) findViewById(R.id.news_details_progressbar);
         webView = (WebView) findViewById(R.id.activity_details_webview);
         int position = getIntent().getIntExtra(KEY_POSITION,DEFAULT_POSITION);
         NewsObjects newsObjects = NewsObjects.getObjNewsObjects().get(position);
+        getSupportActionBar().setTitle(newsObjects.getTitle());
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient(){});
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
         webView.loadUrl(newsObjects.getDetailsUrl());
     }
 
