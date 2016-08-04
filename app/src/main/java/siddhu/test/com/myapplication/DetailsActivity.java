2b1c587bf375f5707b539ec539a7d94edeb6a60a
shdhumale@gmail.com <shdhumale@gmail.com>
2016-08-04 15:19:34 +0530

    Siddhu Commit

---
+++ app/src/main/java/siddhu/test/com/myapplication/DetailsActivity.java
@@ -0,0 +1,18 @@
+package siddhu.test.com.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by admin on 8/4/2016.
+ */
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_detail);
+    }
+
+}
